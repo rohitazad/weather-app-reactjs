8544a5ef893819fccf9d7e669e1dde58abb3bf26
rohitazad <ram9953878727@gmail.com>
2022-10-08 21:58:49 +0530

    PWA create site

--- src/App.js
+++ src/App.js
@@ -11,3 +11,50 @@ function App() {
 }
 
 export default App;
+// Manifest
+// Service worker
+// https://
+// favicon.icon
+// maskable icon
+// APP name
+// install swr
+// npx create-react-app my-app --template cra-template-pwa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
+
+
+
+
+
+
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
